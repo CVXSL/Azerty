1c2744acb3ae5c6d15aa6203aec8cb56a9174905
CVXSL <70546159+CVXSL@users.noreply.github.com>
2021-07-26 20:06:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const prefix = "[Your Prefix]"
+const prefix = "="
 
 // THIS IS THE STATUS
-
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("[your custom status]", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
-        type: "STREAMING", // THIS IS THE STATUS TYPE
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // THIS IS THE LINK IT WILL BRING YOU TO WHEN YOU CLICK "Watch Stream"!
+    bot.user.setActivity(`${bot.users.cache.size} Users in ${bot.guilds.cache.size} Servers!`, { 
+        type: "WATCHING", 
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
 
 }
 )
 
-// THIS IS THE ([prefix]help) COMMAND
 
-// [SCRIPT GOES HERE]
 
 // THIS IS THE bot.login
-
 bot.login(process.env.token);
